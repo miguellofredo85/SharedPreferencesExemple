e9805d935013505556ce3adfb53f86854964cdff
Miguel Lofredo <miguellofredo@gmail.com>
2023-07-18 01:17:18 -0300

    Initial commit: Layout

---
+++ app/src/main/java/com/example/sharedpreferencesexemple/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.sharedpreferencesexemple;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button button;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        
+    }
+}
